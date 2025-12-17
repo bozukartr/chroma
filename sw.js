7a75710465e9c3e0fe072a8c1213faa13d0eb92d
Burak Göl <burak22286@gmail.com>
2025-12-18 02:23:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -36,8 +36,4 @@ self.addEventListener('fetch', (e) => {
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
-});
+
